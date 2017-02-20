3faa4a2491e5cdb909b6290393f97e28f980f970
Jun-PC\Jun <lcjltj@gmail.com>
2017-02-20 11:42:43 +0900

    zz

---
+++ src/main/webapp/resources/js_each/catalog.js
@@ -0,0 +1,27 @@
+$(".login_toggle").click(function() {
+	$(".loginBar").stop().slideToggle(300);
+});
+
+$(".gBtn").click(function() {
+	$(".loginBar").stop().slideToggle(300);
+});
+
+// 로그인
+$("#loginBtn").click(function() {
+	var formData = $("#login").serialize();
+	$.ajax({
+		url : "/dvd/users/login.do",
+		type : "post",
+		data : formData,
+		success : function(data) {
+			if (data) {
+				location.reload();
+			} else {
+			}
+		}
+	});
+});
+
+$(".sideBtn").click(function() {
+	$(window).scrollTop(0);
+});
